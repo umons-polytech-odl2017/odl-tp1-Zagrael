41df3da3ba5251e008a514d3d4dc78c76204cc03
Zagrael <aymerick-soyez@outlook.com>
2017-10-26 19:24:36 +0200

    Résolution finale du TP 1

--- src/main/java/exercise3/Point.java
+++ src/main/java/exercise3/Point.java
@@ -4,16 +4,25 @@ public class Point {
 	private int x = 0;
 	private int y = 0;
 
-	// Constructeur
+	// Constructeurs
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+	public Point(Point p) {
+		this.x = p.x();
+		this.y = p.y();
 	}
 
 	// Accesseurs
 	public int x() { return x; }
 	public int y() { return y; }
 
-	public void setAbscisse(int x) { this.x = x; }
-	public void setOrdonnee(int y) { this.y = y; }
+	@Override
+	public String toString() {
+		return "Point{" +
+			"x=" + x +
+			", y=" + y +
+			'}';
+	}
 }
--- src/main/java/exercise3/Rectangle.java
+++ src/main/java/exercise3/Rectangle.java
@@ -1,10 +1,41 @@
 package exercise3;
 
-public class Rectangle {
-	Point sommets[];
+import java.util.Arrays;
+
+public class Rectangle extends ConvexPolygon {
+	private int m_largeur = 0;
+	private int m_longueur = 0;
 
 	// Constructeur
-	public Rectangle() {
+	public Rectangle() {}
+	public Rectangle(Point A, Point B) {
+		sommets = new Point[4];
+		sommets[0] = A;
+		sommets[1] = new Point(A.x(), B.y());
+		sommets[2] = B;
+		sommets[3] = new Point(B.x(), A.y());
 
+		m_largeur = (Math.abs(B.x() - A.x()) <= Math.abs(B.y() - A.y())) ? Math.abs(B.x() - A.x()) : Math.abs(B.y() - A.y());
+		m_longueur = (Math.abs(B.x() - A.x()) >= Math.abs(B.y() - A.y())) ? Math.abs(B.x() - A.x()) : Math.abs(B.y() - A.y());
+	}
+
+	// Méthodes
+	@Override
+	public double perimetre() {
+		return 2 * (m_largeur + m_longueur);
+	}
+
+	@Override
+	public double aire() {
+		return m_largeur * m_longueur;
+	}
+
+	@Override
+	public String toString() {
+		return "Rectangle{" +
+			"m_largeur=" + m_largeur +
+			", m_longueur=" + m_longueur +
+			", sommets=" + Arrays.toString(sommets) +
+			'}';
 	}
 }
